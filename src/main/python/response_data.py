0ebb14f2cc665a11761ed24daa1cd9888a6e5f92
Udayaprasad <Udayaprasad@users.noreply.github.com>
2019-11-29 15:47:44 +0530

    First Commit
    First Commit

---
+++ src/main/python/response_data.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 22:32:46 2019
+
+@author: uvakalapudi
+"""
+import json
+
+class APIPosResponse(object):
+    def __init__(self, character_name, movie_name, quotes):
+        self.character_name = character_name
+        self.movie_name = movie_name
+        self.quotes = quotes
+    
+    def convertToJSON(self):
+        return_response = {
+            "character_name": self.character_name,
+            "movie_name": self.movie_name,
+            "quotes": self.quotes
+            }
+        return json.dumps(return_response)
+
+class APINegResponse(object):
+    def __init__(self, request_type, response_codes):
+        self.request_type = request_type
+        self.response_codes = response_codes
+    
+    def convertToJSON(self):
+        return_response = {
+            "request_type": self.request_type,
+            "response_codes": self.response_codes
+            }
+        return json.dumps(return_response)
+        
+        
